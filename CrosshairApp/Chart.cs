c916e5ac5259e31f8cb09a81e27756e846c626e6
JohanLarsson <johan_gavle1@hotmail.com>
2021-09-20 18:31:20 +0200

    Slow now

--- CrosshairApp/Chart.cs
+++ CrosshairApp/Chart.cs
@@ -8,6 +8,7 @@
     public sealed class Chart : FrameworkElement
     {
         private readonly Crosshair crosshair = new();
+        private Pen? pen;
 
         public Chart()
         {
@@ -27,6 +28,29 @@
         protected override void OnPreviewMouseMove(MouseEventArgs e)
         {
             this.crosshair.Position(e.GetPosition(this), this.RenderSize);
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            this.pen ??= CreatePen();
+            for (var i = 0; i < 1000; i++)
+            {
+                drawingContext.DrawRectangle(Brushes.Red, null, new Rect(new Point(i, i), new Size(1, 1)));
+                drawingContext.DrawRectangle(Brushes.Blue, null, new Rect(new Point(i + 2, i + 2), new Size(2, 2)));
+                drawingContext.DrawRectangle(Brushes.Yellow, null, new Rect(new Point(i + 3, i + 3), new Size(3, 3)));
+                drawingContext.DrawRectangle(Brushes.HotPink, null, new Rect(new Point(i + 4, i + 4), new Size(4, 4)));
+                drawingContext.DrawLine(this.pen, new Point(i, i + 1), new Point(i - 1, i));
+                drawingContext.DrawLine(this.pen, new Point(i, i + 2), new Point(i - 1, i));
+                drawingContext.DrawLine(this.pen, new Point(i, i + 3), new Point(i - 1, i));
+                drawingContext.DrawLine(this.pen, new Point(i, i + 4), new Point(i - 1, i));
+            }
+
+            static Pen CreatePen()
+            {
+                var pen = new Pen(Brushes.Black, 0.5);
+                pen.Freeze();
+                return pen;
+            }
         }
     }
 }
